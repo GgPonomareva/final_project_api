1b9d8b89be523d93b5ea5cb9060921394c2e4931
Gg123 <li.c@mail.ru>
2023-05-23 00:39:05 +0300

    Initial commit

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,20 +1,24 @@
 package tests;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import models.UserGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static specs.Specs.request;
-import static specs.Specs.response200;
+import static specs.Specs.*;
 
 @Epic("API Reqres")
 @Feature("User authorization")
-
 public class LoginTests {
+
     @Test
+    @DisplayName("Successful login")
+    @Description("POST /api/login")
     void successLoginTest() {
 
         UserGenerator user = UserGenerator.builder()
@@ -28,8 +32,27 @@ public class LoginTests {
                 .when()
                 .post("/login")
                 .then()
-                .log().all()
                 .spec(response200)
                 .body("token", is(notNullValue()));
+    }
+
+    @Test
+    @DisplayName("Unsuccessful login with incorrect user data")
+    @Description("POST /api/login")
+    void unSuccessLoginTest() {
+
+        UserGenerator user = UserGenerator.builder()
+                .email("test.holt@reqres.in")
+                .password("pass")
+                .build();
+
+        given()
+                .spec(request)
+                .body(user)
+                .when()
+                .post("/login")
+                .then()
+                .spec(response400)
+                .body("error", is("user not found"));
     }
 }
